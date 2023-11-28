151c621ab21f3121734796db08adb2f4c04eb45a
Amor Koloskova <amor.koloskova@index.ru>
2023-11-28 12:35:03 +0300

    Initial Commit

---
+++ To-Do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do
+//
+//  Created by Amor on 28.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
